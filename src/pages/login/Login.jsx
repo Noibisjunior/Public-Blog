5e413d07dda9f273f4aca8bdba854c11dd6c099b
Noibisjunior <noibisjunior22@gmail.com>
2023-04-21 21:17:08 +0100

    thanks

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,31 +1,94 @@
-import React from 'react';
 import './login.css';
 import back from '../../assets/images/login.jpg';
+import  { useContext, useState } from 'react';
+import userContext from './userContext';
+import { useNavigate } from 'react-router-dom';
 
- const Login = () => {
+const LoginPage = () => {
+  const [userName, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [redirect, setRedirect] = useState(false);
+  const { setUserInfo } = useContext(userContext);
+
+  const Navigate = useNavigate();
+
+  async function login(e) {
+    e.preventDefault();
+    const response = await fetch('http://localhost:5000/auth/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ userName, password }),
+    });
+    if (response.ok) {
+      response.json().then((userInfo) => {
+        setUserInfo(userInfo);
+        setRedirect(true);
+      });
+    } else {
+      alert('wrong credentials');
+    }
+  }
+
+  if (redirect) {
+    return <Navigate to={'/'} />;
+  }
+
   return (
-    <>
-      <section className="login">
-        <div className="container">
-          <div className="backImg">
-            <img src={back} alt="" />
-            <div className="text">
-              <h3>Login</h3>
-              <h1>My account</h1>
-            </div>
-          </div>
-
-          <form>
-            <span>Username or email address *</span>
-            <input type="text" required />
-            <span>Password *</span>
-            <input type="password" required />
-            <button className="button">Log in</button>
-          </form>
-        </div>
-      </section>
-    </>
+    <form className="login" onSubmit={login}>
+     <div className="container">
+           <div className="backImg">
+             <img src={back} alt="img" />
+             <div className="text">
+      <h1>Login</h1>
+      </div>
+      </div>
+      <input
+        type="text"
+        placeholder="username"
+        value={userName}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button className='button'>Login</button>
+      </div>
+    </form>
   );
 };
 
-export default Login;
+export default LoginPage;
+
+
+
+
+//  const Login = () => {
+//   return (
+//     <>
+//       <section className="login">
+//         <div className="container">
+//           <div className="backImg">
+//             <img src={back} alt="" />
+//             <div className="text">
+//               <h3>Login</h3>
+//               <h1>My account</h1>
+//             </div>
+//           </div>
+
+//           <form>
+//             <span>Username or email address *</span>
+//             <input type="text" required />
+//             <span>Password *</span>
+//             <input type="password" required />
+//             <button className="button">Log in</button>
+//           </form>
+//         </div>
+//       </section>
+//     </>
+//   );
+// };
+
+// export default Login;
--- src/pages/login/Register.jsx
+++ src/pages/login/Register.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import  { useState } from 'react'
 import './login.css';
 import back from '../../assets/images/register.jpg';
 
- const Regsiter = () => {
+
+const Register = () => {
+ const [email,setEmail] = useState('')
+ const [username,setUsername] = useState('')
+ const [password,setPassword] = useState('')
+
+ async function register(event){
+  event.preventDefault(); //prevent the default behaviour of the browser
+
+const response = await fetch('http://localhost:5000/auth/register', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({ email,username, password }),
+});
+// const result = await response.json()
+console.log(response)
+
+if(response.status === 200) {
+  alert('registration successful')
+}else{
+  alert('registration failed')
+}
+ }
+
+
   return (
-    <>
-      <section className="login">
+     <section className="login">
         <div className="container">
           <div className="backImg">
             <img src={back} alt="" />
@@ -13,23 +36,71 @@ import back from '../../assets/images/register.jpg';
               <h3>Register</h3>
               <h1>My account</h1>
             </div>
-          </div>
-
-          <form>
-            <span>Email address *</span>
-            <input type="text" required />
-            <span>Username *</span>
-            <input type="text" required />
-            <span>Password *</span>
-            <input type="password" required />
-            <span>Conform Password *</span>
-            <input type="password" required />
-            <button className="button">Register</button>
-          </form>
-        </div>
-      </section>
-    </>
+            </div>
+    <form onSubmit={register}>
+      <h1>Register</h1>
+      <input
+        type="email"
+        placeholder="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button className='button'>Register</button>
+    </form>
+    </div>
+    </section>
   );
-};
+}
 
-export default Regsiter
+export default Register;
+
+
+
+
+
+
+// import React from 'react';
+
+//  const Regsiter = () => {
+//   return (
+//     <>
+//       <section className="login">
+//         <div className="container">
+//           <div className="backImg">
+//             <img src={back} alt="" />
+//             <div className="text">
+//               <h3>Register</h3>
+//               <h1>My account</h1>
+//             </div>
+//           </div>
+
+//           <form>
+//             <span>Email address *</span>
+//             <input type="text" required />
+//             <span>Username *</span>
+//             <input type="text" required />
+//             <span>Password *</span>
+//             <input type="password" required />
+//             <span>Conform Password *</span>
+//             <input type="password" required />
+//             <button className="button">Register</button>
+//           </form>
+//         </div>
+//       </section>
+//     </>
+//   );
+// };
+
+// export default Regsiter
